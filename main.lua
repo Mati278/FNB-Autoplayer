26a1259bcc14ac5fa550a45873e61f7bb0226a8e
Mati278 <100154995+Mati278@users.noreply.github.com>
2022-07-25 13:01:45 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -30,7 +30,7 @@ local CreditsFolder = Window:AddFolder("Credits")
 
 local toggle = FolderMain:AddToggle({text = "AutoPlayer", flag = "yes", state = true})
 
-FolderMain:AddBind({ text = 'Autoplayer toggle', flag = 'yes', key = Enum.KeyCode.End, callback = function() toggle:SetState(not toggle.state) end})
+FolderMain:AddBind({ text = 'Autoplayer toggle', flag = 'yes', key = Enum.KeyCode.Start, callback = function() toggle:SetState(not toggle.state) end})
 FolderMain:AddSlider({ text= 'Bot accuracy (ms)',flag = "ms", min = -100, max = 100, value = 31.7})
 
 Window:AddBind({ text = "Hide/show menu", key = Enum.KeyCode.Delete, callback = function() uwuware:Close() end})
