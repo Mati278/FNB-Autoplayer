a0b1dd45f4e3b9fd88c22d82814a96d4404a98a0
Mati278 <100154995+Mati278@users.noreply.github.com>
2022-12-07 20:54:53 -0500

    Splash Texts! (suggestions on PRs)

--- main.lua
+++ main.lua
@@ -13,6 +13,17 @@ local connections = {
         table.clear(self)
     end
 };
+local SplashIndex = math.random(1,8)
+local SplashText
+
+if SplashIndex == 1 then SplashText = 'hi guys hows doin there' end
+if SplashIndex == 2 then SplashText = 'when Blue Pokemon (Encore) - Vs. Documic.txt Reformatted OST is going to be added' end
+if SplashIndex == 3 then SplashText = 'Did u know that that in terms of male human and female pokemon breeding...' end
+if SplashIndex == 4 then SplashText = 'They took everything from me: my voice, my freedom, my legacy; and they replaced me with some blue haired kid' end
+if SplashIndex == 5 then SplashText = 'discord.gg/pizzahut worst place ever' end
+if SplashIndex == 6 then SplashText = 'amogus ඞ' end
+if SplashIndex == 7 then SplashText = 'Go play PFN instead' end
+if SplashIndex == 8 then SplashText = 'swish i fixed the autoplayer for u :)' end
 
 local Players = game:GetService'Players'
 local Client = Players.LocalPlayer
@@ -31,7 +42,7 @@ local Keys = {
 
 local set_identity = (syn and syn.set_thread_identity or setidentity or setthreadcontext);
 local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
-local Window = Library:MakeWindow({IntroText = "Mati278's Funny FNB Autoplayer v2.8",Name = "Friday Night Bloxxin' Autoplayer", HidePremium = true, SaveConfig = false})
+local Window = Library:MakeWindow({IntroText = tostring(SplashText),Name = "Friday Night Bloxxin' Autoplayer", HidePremium = true, SaveConfig = false})
 local Folder = Window:MakeTab({Name = "Main", Icon = "rbxassetid://4483345998", PremiumOnly = false})
 local CreditsFolder = Window:MakeTab({Name = "Credits", Icon = "rbxassetid://2484564290", PremiumOnly = false})
 local ExtrasFolder = Window:MakeTab({Name = "Extras", Icon = "rbxassetid://7468828225", PremiumOnly = false})
