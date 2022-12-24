112c1f2eee0f8ec555b804a6a83c9efb0f54ddd6
Mati278 <100154995+Mati278@users.noreply.github.com>
2022-12-24 11:02:22 -0500

    Update settingChanger.lua

--- settingChanger.lua
+++ settingChanger.lua
@@ -10,7 +10,12 @@ for i,v in pairs(game:GetService("ReplicatedStorage").Misc:GetDescendants()) do 
     end
 end
 
-
-
-
-game:GetService'ReplicatedStorage'.Events.RemoteEvent:FireServer("Input", 0, "ScrollSpeed")
+local SettingValue
+local Setting
+local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
+local Window = Library:MakeWindow({IntroText = yes,Name = "fnb setting thing", HidePremium = true})
+local Folder = Window:MakeTab({Name = "Main", Icon = "rbxassetid://4483345998", PremiumOnly = false})
+Folder:AddTextbox({Name = "Setting value", Default = "1", extDisappear = false, Callback = function(Value) SettingValue = Value end})
+Folder:AddTextbox({Name = "Setting to change", Default = "ScrollSpeed", extDisappear = false, Callback = function(Value) SettingValue = Value end})
+Folder:AddButton({Name = "Apply changes", Callback = function() game:GetService'ReplicatedStorage'.Events.RemoteEvent:FireServer("Input", tostring(SettingValue), tostring(Setting)) Library:MakeNotification({Name = "Done!", Content = "Changes applied. If changes didn't apply the game tried to ban u", Image = "rbxassetid://8370951784", Time = 3}) end})
+Library:Init()
