6ca1ad8a57ba8bfeb4224516ae0899303bccb71b
Mati278 <100154995+Mati278@users.noreply.github.com>
2022-12-29 21:22:08 -0500

    awesome

--- blacklist.lua
+++ blacklist.lua
@@ -1,1 +1,11 @@
-{506813014, 3267163772}
+local LP = game:GetService'Players'.LocalPlayer
+local id = LP.userId
+
+local function GetFuckedLmao()
+    game:GetService("ReplicatedStorage").Events.RemoteEvent:FireServer("Input", "fuck u", "Points")
+    task.wait(.5)
+    LP:Kick(string.format('You deserve it, dirty cheater. If the fnb devs can't do it , i will'))
+end  
+if id == 506813014 then
+    GetfuckedLmao()
+end        
